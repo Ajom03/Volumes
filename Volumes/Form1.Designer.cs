cb9c1c2e967b1a8e5043a41cd296afba8c132605
Aidan Machell <rc0037292@student.riversidecollege.ac.uk>
2020-01-07 11:05:18 +0000

    Add project files.

---
+++ Volumes/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Volumes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Series1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.S1R1 = new System.Windows.Forms.TextBox();
+            this.S1R2 = new System.Windows.Forms.TextBox();
+            this.S2R1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.S2R2 = new System.Windows.Forms.TextBox();
+            this.S2R3 = new System.Windows.Forms.TextBox();
+            this.P1R1 = new System.Windows.Forms.TextBox();
+            this.P1R2 = new System.Windows.Forms.TextBox();
+            this.P2R1 = new System.Windows.Forms.TextBox();
+            this.P2R2 = new System.Windows.Forms.TextBox();
+            this.P2R3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(409, 296);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(379, 125);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 276);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(295, 145);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(502, 32);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(269, 161);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Series1
+            // 
+            this.Series1.Location = new System.Drawing.Point(32, 32);
+            this.Series1.Name = "Series1";
+            this.Series1.Size = new System.Drawing.Size(238, 147);
+            this.Series1.TabIndex = 0;
+            this.Series1.TabStop = false;
+            this.Series1.Click += new System.EventHandler(this.Series1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(32, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 147);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // S1R1
+            // 
+            this.S1R1.Location = new System.Drawing.Point(32, 185);
+            this.S1R1.Name = "S1R1";
+            this.S1R1.Size = new System.Drawing.Size(100, 20);
+            this.S1R1.TabIndex = 4;
+            this.S1R1.TextChanged += new System.EventHandler(this.S1R1_TextChanged);
+            // 
+            // S1R2
+            // 
+            this.S1R2.Location = new System.Drawing.Point(153, 185);
+            this.S1R2.Name = "S1R2";
+            this.S1R2.Size = new System.Drawing.Size(100, 20);
+            this.S1R2.TabIndex = 5;
+            // 
+            // S2R1
+            // 
+            this.S2R1.Location = new System.Drawing.Point(396, 32);
+            this.S2R1.Name = "S2R1";
+            this.S2R1.Size = new System.Drawing.Size(100, 20);
+            this.S2R1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(350, 215);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // S2R2
+            // 
+            this.S2R2.Location = new System.Drawing.Point(396, 104);
+            this.S2R2.Name = "S2R2";
+            this.S2R2.Size = new System.Drawing.Size(100, 20);
+            this.S2R2.TabIndex = 8;
+            // 
+            // S2R3
+            // 
+            this.S2R3.Location = new System.Drawing.Point(396, 173);
+            this.S2R3.Name = "S2R3";
+            this.S2R3.Size = new System.Drawing.Size(100, 20);
+            this.S2R3.TabIndex = 9;
+            // 
+            // P1R1
+            // 
+            this.P1R1.Location = new System.Drawing.Point(32, 250);
+            this.P1R1.Name = "P1R1";
+            this.P1R1.Size = new System.Drawing.Size(100, 20);
+            this.P1R1.TabIndex = 10;
+            // 
+            // P1R2
+            // 
+            this.P1R2.Location = new System.Drawing.Point(153, 250);
+            this.P1R2.Name = "P1R2";
+            this.P1R2.Size = new System.Drawing.Size(100, 20);
+            this.P1R2.TabIndex = 11;
+            // 
+            // P2R1
+            // 
+            this.P2R1.Location = new System.Drawing.Point(409, 270);
+            this.P2R1.Name = "P2R1";
+            this.P2R1.Size = new System.Drawing.Size(100, 20);
+            this.P2R1.TabIndex = 12;
+            // 
+            // P2R2
+            // 
+            this.P2R2.Location = new System.Drawing.Point(548, 270);
+            this.P2R2.Name = "P2R2";
+            this.P2R2.Size = new System.Drawing.Size(100, 20);
+            this.P2R2.TabIndex = 13;
+            // 
+            // P2R3
+            // 
+            this.P2R3.Location = new System.Drawing.Point(688, 270);
+            this.P2R3.Name = "P2R3";
+            this.P2R3.Size = new System.Drawing.Size(100, 20);
+            this.P2R3.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.P2R3);
+            this.Controls.Add(this.P2R2);
+            this.Controls.Add(this.P2R1);
+            this.Controls.Add(this.P1R2);
+            this.Controls.Add(this.P1R1);
+            this.Controls.Add(this.S2R3);
+            this.Controls.Add(this.S2R2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.S2R1);
+            this.Controls.Add(this.S1R2);
+            this.Controls.Add(this.S1R1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.Series1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Series1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox S1R1;
+        private System.Windows.Forms.TextBox S1R2;
+        private System.Windows.Forms.TextBox S2R1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox S2R2;
+        private System.Windows.Forms.TextBox S2R3;
+        private System.Windows.Forms.TextBox P1R1;
+        private System.Windows.Forms.TextBox P1R2;
+        private System.Windows.Forms.TextBox P2R1;
+        private System.Windows.Forms.TextBox P2R2;
+        private System.Windows.Forms.TextBox P2R3;
+    }
+}
+
---
+++ Volumes/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Volumes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void R1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void S1R1_TextChanged(object sender, EventArgs e)
+        {
+            double ResistorSeries1 = double.Parse(S1R1.Text);
+            double ResistorSeries2 = double.Parse(S1R2.Text);
+            double ResistorParallel1 = double.Parse(P1R1.Text);
+            double ResistorParallel2 = double.Parse(P1R2.Text);
+            double ResistorSeries3 = double.Parse(S2R1.Text);
+            double ResistorSeries4 = double.Parse(S2R2.Text);
+            double ResistorSeries5 = double.Parse(S2R3.Text);
+            double ResistorParallel3 = double.Parse(P2R1.Text);
+            double ResistorParallel4 = double.Parse(P2R2.Text);
+            double ResistorParallel5 = double.Parse(P2R3.Text);
+        }
+
+        private void Series1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
